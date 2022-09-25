c4db27a39f83713fecc5481f5af0d8052f1294c7
Kurgansky Maxim <it.maxk@gmail.com>
2022-09-25 17:32:50 +0300

    Blog

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+import { registerValidation, loginValidation, postCreateValidation } from './validations/validations.js';
+import UserModel from './models/User.js';
+import checkAuth from './utils/checkAuth.js';
+import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
+
+
+mongoose
+    .connect('mongodb+srv://admin:Test12345@cluster0.y8cuxjh.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
+
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/login', loginValidation, UserController.login);
+
+app.post('/auth/register', registerValidation, UserController.register);
+
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    };
+
+    console.log('Server OK');
+});
+
+
+
